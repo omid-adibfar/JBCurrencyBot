0d977ddd558ec904870624c7bef0c2f18c18da42
Omiid Adibfar <omid.adibfar@pomechain.com>
2023-03-13 11:31:38 +0330

    separate currency message for more readability

--- bot.py
+++ bot.py
@@ -44,6 +44,39 @@ def get_command_details():
     return "\n\n/help - Get help\n/update - Get latest price of currencies and converts them to IRR"
 
 
+def get_currency_text():
+    return """{}
+
+درهم امارات🇦🇪 خريد: {}🔹فروش: {}
+|
+دلار امریکا🇺🇸 خريد: {}🔹فروش: {}
+|
+حواله دلار🇺🇸 خريد: {}🔹فروش: {}
+|
+یورو🇪🇺 خريد: {}🔹فروش: {}
+|
+حواله یورو🇪🇺 خريد: {}🔹فروش: {}
+|
+پوند انگلیس🇪🇺 خريد: {}🔹فروش: {}
+|
+دلار کانادا🇨🇦 خريد: {}🔹فروش: {}
+|
+یوان چین🇨🇳 خريد: {}🔹فروش: {}
+|
+دلار استرالیا🇦🇺 خريد: {}🔹فروش: {}
+|""".format(datetime.datetime.now().strftime("%#d %b"),  # use hyphen instead of hashtag in Unix systems to skip zero
+            buy_price(AED), sell_price(AED),
+            buy_price(USD), sell_price(USD),
+            buy_price(USDR), sell_price(USDR),
+            buy_price(EUR), sell_price(EUR),
+            buy_price(EURR), sell_price(EURR),
+            buy_price(POUND), sell_price(POUND),
+            buy_price(CAD), sell_price(CAD),
+            buy_price(CNY), sell_price(CNY),
+            buy_price(AUD), sell_price(AUD),
+            )
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     logger.debug("Bot started.")
@@ -72,40 +105,7 @@ def update_prices(message):
         msg.id = CHANNEL_EDITABLE_MESSAGE_ID
     try:
         bot.edit_message_text(chat_id=CHANNEL_CHAT_ID,
-                              text="""
-                            {}
-
-    درهم امارات🇦🇪 خريد: {}🔹فروش: {}
-    |
-    دلار امریکا🇺🇸 خريد: {}🔹فروش: {}
-    |
-    حواله دلار🇺🇸 خريد: {}🔹فروش: {}
-    |
-    یورو🇪🇺 خريد: {}🔹فروش: {}
-    |
-    حواله یورو🇪🇺 خريد: {}🔹فروش: {}
-    |
-    پوند انگلیس🇪🇺 خريد: {}🔹فروش: {}
-    |
-    دلار کانادا🇨🇦 خريد: {}🔹فروش: {}
-    |
-    یوان چین🇨🇳 خريد: {}🔹فروش: {}
-    |
-    دلار استرالیا🇦🇺 خريد: {}🔹فروش: {}
-    |
-                            """.format(
-                                  datetime.datetime.now().strftime(
-                                      "%#d %b"),  # use hyphen instead of hashtag in Unix systems to skip zero
-                                  buy_price(AED), sell_price(AED),
-                                  buy_price(USD), sell_price(USD),
-                                  buy_price(USDR), sell_price(USDR),
-                                  buy_price(EUR), sell_price(EUR),
-                                  buy_price(EURR), sell_price(EURR),
-                                  buy_price(POUND), sell_price(POUND),
-                                  buy_price(CAD), sell_price(CAD),
-                                  buy_price(CNY), sell_price(CNY),
-                                  buy_price(AUD), sell_price(AUD),
-                              ), message_id=msg.id)
+                              text=get_currency_text(), message_id=msg.id)
     except Exception as e:
         logger.error('An Exception Occurred', exc_info=e)
 
